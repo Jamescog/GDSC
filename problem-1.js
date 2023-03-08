cc1a79923880943c5bb987ef11cbb3214abc5b1c
Jamescog <jamescog72@gmail.com>
2023-03-08 10:31:57 +0300

    making the code more professional!

--- problem-1.js
+++ problem-1.js
@@ -1,24 +1,33 @@
 #!/usr/bin/node
 
-
- /**
-     * @param {number} n - the number
-     * Returns: - "Fizz" -if the number is multiple of 3
-     *          - "Buzz" -if the number is the multiple of 5
-     *          - "FizzBuzz" - if the number is divisible by both 3 and 5
-     * 
-     * Author: Yaekob Demisse - March 8 2023.
-     */
+/**
+ * Returns a string message based on the given number.
+ *
+ * @param {number} n - the number to evaluate
+ * @return {string} - "Fizz" if the number is divisible by 3,
+ *                    "Buzz" if the number is divisible by 5,
+ *                    "FizzBuzz" if the number is divisible by both 3 and 5,
+ *                    the number itself as a string otherwise
+ *
+ * Author: Yaekob Demisse - March 8 2023;
+ */
 
 const fizzBuzz = (n) => {
-   if (n % 3 == 0 && n % 5 == 0) {
-        console.log("FizzBuzz")
-   } else if (n % 3 == 0) {
-        console.log("Fizz")
-   }
-   else if (n % 5 == 0) {
-        console.log("Buzz")
-   }
+    if (n % 3 == 0 && n % 5 == 0) {
+        return "FizzBuzz";
+    } else if (n % 3 == 0) {
+        return "Fizz";
+    } else if (n % 5 == 0) {
+        return "Buzz";
+    } else {
+        return n.toString();
+    }
 }
 
 export default fizzBuzz;
+
+// Example usage:
+// console.log(fizzBuzz(3)); // Output: "Fizz"
+// console.log(fizzBuzz(5)); // Output: "Buzz"
+// console.log(fizzBuzz(15)); // Output: "FizzBuzz"
+// console.log(fizzBuzz(7)); // Output: "7"
