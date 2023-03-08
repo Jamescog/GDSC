49e8ba1a3ce1363a69ded4172b5472c967e82db6
Jamescog <jamescog72@gmail.com>
2023-03-08 10:36:00 +0300

    making the code more professional!

--- problem-1.js
+++ problem-1.js
@@ -13,14 +13,19 @@
  */
 
 const fizzBuzz = (n) => {
-    if (n % 3 == 0 && n % 5 == 0) {
-        return "FizzBuzz";
-    } else if (n % 3 == 0) {
-        return "Fizz";
-    } else if (n % 5 == 0) {
-        return "Buzz";
+    if (!Number.isInteger(n)) {
+        throw new Error("Parameter must be an integer type")
     } else {
-        return n.toString();
+
+        if (n % 3 == 0 && n % 5 == 0) {
+            return "FizzBuzz";
+        } else if (n % 3 == 0) {
+            return "Fizz";
+        } else if (n % 5 == 0) {
+            return "Buzz";
+        } else {
+            return n.toString();
+        }
     }
 }
 
